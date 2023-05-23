d944ff4aa47fcc18530dd4b9d52d0d617672353a
Martin D'Agostino <martin.dagostino@klarna.com>
2023-05-23 16:42:37 +0200

    temp clone and initial schema gen

--- repo-1/scripts/generate-schema.py
+++ repo-1/scripts/generate-schema.py
@@ -1,2 +1,11 @@
 if __name__ == '__main__':
+    # create a json schema from a yaml file
+    import yaml
+    import json
+
+    with open("repo-1/metadata.yml", 'r') as file:
+        metadata = yaml.safe_load(file)
+        with open("repo-1/schema.json", 'w') as file:
+            json.dump(metadata['request'], file, indent=4)
+
     print("new schema generated")
